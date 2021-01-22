e931c9faa81bca5d38860a566db4ca59b910d04b
David Dumas <david@dumas.io>
2021-01-22 10:33:09 -0600

    Additional features in preparation for Lecture 5

--- samplecode/plane.py
+++ samplecode/plane.py
@@ -1,6 +1,13 @@
-"""Geometric objects in the plane"""
+"""Points and vectors in the plane"""
 # MCS 275 Spring 2021 David Dumas
-# Lecture 4
+# Lectures 4 and 5
+
+# This version of the plane module includes additional features
+# added between Lecture 4 and Lecture 5.
+
+# The earlier version of this module that was created in the live
+# coding part of Lecture 4 can be found at:
+# https://github.com/daviddumas/mcs275spring2021/blob/ba4a305212a7a15a4c068368e14f74a58c81c76f/samplecode/plane.py
 
 class Point:
     """A point in the plane"""
@@ -16,6 +23,20 @@ class Point:
     def __repr__(self):
         """String representation of a Point"""
         return str(self)
+
+    def __sub__(self,other):
+        """Point-Point gives displacement vector"""
+        if isinstance(other,Point):
+            return Vector(self.x-other.x,self.y-other.y)
+        else:
+            return NotImplemented
+
+    def __eq__(self,other):
+        """Point equality means coordinate equality"""
+        if isinstance(other,Point):
+            return self.x==other.x and self.y==other.y
+        else:
+            return False
 
 class Vector:
     """A 2-dimensional vector"""
@@ -54,3 +75,25 @@ class Vector:
             return Vector(self.x-other.x,self.y-other.y)
         else:
             return NotImplemented
+
+    def __mul__(self,other):
+        """Scalar multiplication (with Vector on the left)"""
+        if isinstance(other, (int,float)):
+            return Vector(self.x*other,self.y*other)
+        else:
+            return NotImplemented
+
+    def __rmul__(self,other):
+        """Scalar multiplication (with Vector on the right)"""
+        return self*other
+
+    def __abs__(self):
+        """Norm of the vector"""
+        return (self.x**2 + self.y**2)**0.5
+
+    def __eq__(self,other):
+        """Vector equality means component equality"""
+        if isinstance(other,Vector):
+            return self.x==other.x and self.y==other.y
+        else:
+            return False
