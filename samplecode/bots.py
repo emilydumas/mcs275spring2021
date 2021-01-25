2495a54e65bd9fa4b54364f40bb8526a3446c880
David Dumas <david@dumas.io>
2021-01-25 13:33:29 -0600

    Lecture 6 updates: PatrolBot and DestructBot

--- samplecode/bots.py
+++ samplecode/bots.py
@@ -1,6 +1,6 @@
 """Classes representing robots in a simulation"""
 # MCS 275 Spring 2021 David Dumas
-# Lecture 5
+# Lectures 5 and 6
 
 from plane import Point,Vector
 import random
@@ -14,7 +14,14 @@ class Bot:
     
     def __str__(self):
         """Human-readable string representation"""
-        return "{}(position={},alive={})".format(self.__class__.__name__,self.position,self.alive)
+        return "{}(position={},alive={})".format(
+            self.__class__.__name__,
+            self.position,
+            self.alive
+        )
+
+        # self.__class__ is Bot
+        # self.__class__.__name__ is "Bot"
 
     def __repr__(self):
         """Unambiguous string representation"""
@@ -25,17 +32,64 @@ class Bot:
 
 class WanderBot(Bot):
     """Robot that wanders randomly"""
+    steps = [ Vector(1,0),
+              Vector(0,1),
+              Vector(-1,0),
+              Vector(0,-1) ]  # Class attribute
     def __init__(self,position):
         """Setup wandering robot with initial position `position`"""
         # Call the constructor of Bot
         super().__init__(position)
         # WanderBot-specific initialization
-        self.steps = [ Vector(1,0),
-                       Vector(0,1),
-                       Vector(-1,0),
-                       Vector(0,-1) ]
     
     def update(self):
         """Take one random step"""
         self.position += random.choice(self.steps)
         #             ^ ends up calling Point.__add__(Vector)
+
+class DestructBot(Bot):
+    """Robot that sits in one place for a while, then self-destructs"""
+    def __init__(self,position,lifetime):
+        """Setup a bot at position `position` that sits for
+        `lifetime` steps before self-destructing."""
+        super().__init__(position)
+        self.lifetime = lifetime # number of steps remaining
+
+    def update(self):
+        """Decrease lifetime by one unit, and self-destruct
+        if it reaches zero."""
+        # We only decrease the lifetime if it is
+        # nonzero.  That way, once it reaches zero,
+        # it stays there.
+        if self.lifetime:
+            self.lifetime -= 1
+
+        # alive should be True if lifetime>0,
+        # or False if lifetime==0.
+        self.alive = bool(self.lifetime)
+
+class PatrolBot(Bot):
+    """Robot that walks back and forth along a fixed route"""
+    def __init__(self,position,direction,nstep):
+        """Setup a bot at position `position` that takes steps
+        along the vector `direction` for `nstep` units of time
+        before turning around and going back.  This cycle
+        repeats forever."""
+        super().__init__(position)
+        self.direction = direction
+        self.nstep = nstep # total length of patrol route (number of steps)
+        self.count = self.nstep # steps left before the next turn
+
+    def update(self):
+        """Take one step, turn around if appropriate"""
+        self.position += self.direction # take one step
+        self.count -= 1
+        if self.count == 0:
+            self.direction = -1*self.direction # turn around
+            self.count = self.nstep
+        
+# TODO: Add __str__ methods in the other Bots that report
+# their vital stats, like lifetime, direction, etc.
+# (The inherited __str__ from Bot only shows position & alive)        
+        
+    
--- samplecode/botsimulation.py
+++ samplecode/botsimulation.py
@@ -1,20 +1,51 @@
 """Robot motion simulation with simple text-based graphics"""
+# MCS 275 Spring 2021 - David Dumas
+# Used in Lecture 5 and Lecture 6
 
 from plane import Vector,Point
 import bots
 import random
+import time
 
 width=60
 height=30
 
 current_bots = []
+
+# Make some wander bots
 for i in range(5):
     P = Point(random.randint(0,width-1),random.randint(0,height-1))
     current_bots.append(bots.WanderBot(position=P))
 
+# Make some patrol bots
+patrol_directions = [ 
+    Vector(1,0),
+    Vector(0,1),
+    Vector(1,1)
+]
+for i in range(10):
+    P = Point(random.randint(0,width-1),random.randint(0,height-1))
+    D = random.choice(patrol_directions)
+    current_bots.append(bots.PatrolBot(position=P,direction=D,nstep=8))
+
+# Make two destruct bots
+current_bots.append(bots.DestructBot(position=Point(4,4),lifetime=5))
+current_bots.append(bots.DestructBot(position=Point(4,10),lifetime=15))
+
+# Symbols for the different kinds of bots
+botsymbols = {
+    bots.PatrolBot: "P",
+    bots.DestructBot: "D",
+    bots.WanderBot: "W",
+    bots.Bot: "*"
+}
+
+print("Press ENTER to begin the simulation")
+input()
+
 n=0
 while True:
-    print("\n"*3*height)
+    print("\n"*2*height)
     board = [ [" "]*width for _ in range(height) ]
     for b in current_bots:
         if not b.alive:
@@ -23,11 +54,20 @@ while True:
             continue
         elif b.position.y < 0 or b.position.y >= height:
             continue
-        board[b.position.y][b.position.x] = "*"
+        # Mark the spot with a symbol depending on bot type
+        board[b.position.y][b.position.x] = botsymbols[b.__class__]
+    
+    # To print the board, we'll print a lot of newlines, then
+    # the board itself, and then the time indicator.  We'll put those
+    # into a single string to reduce the chance that part of the display
+    # is updated before the whole thing is shown.  This makes the
+    # "graphics" a little more fluid.
+    boardstr = "\n"*3*height
     for row in board:
-        print("".join(row))
-    print("time={}".format(n))
-    input()
+        boardstr+="".join(row) + "\n"
+    boardstr += "time={}".format(n)
+    print(boardstr,flush=True)
+    time.sleep(0.2)
 
     for b in current_bots:
         b.update()
