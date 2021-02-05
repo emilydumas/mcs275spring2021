c9f3b5c6be90402badbad8e851e4d051d92201b0
David Dumas <david@dumas.io>
2021-02-05 14:52:41 -0600

    Revise to solve debugging challenge from lecture 10

--- samplecode/debugging/letterhist.py
+++ samplecode/debugging/letterhist.py
@@ -1,7 +1,7 @@
 """Read a file containing a word list, and display a histogram of
 first letters.  (This is the post-debugging version.)"""
 # MCS 275 Spring 2021 - David Dumas
-# Lecture 10
+# Lecture 10 and 11
 
 # The files "words1.txt", "words2.txt", ... are sample inputs
 # for this program.
@@ -12,20 +12,23 @@ def first_letter_hist(wordlist):
     """Take a list of words, `wordlist`, and return a
     dict that gives a histogram of first letter
     frequencies (converting everything to lowercase)."""
-    alphabet = "abcdefghijklmnopqrstuvwxyz"
-    counts = { c:0 for c in alphabet }
+    print(wordlist)
+    counts = dict()
     for word in wordlist:
-        # Uncomment the next line for print() debugging
-        #print("Processing word:",word)
         first_char = word[0]
         first_char = first_char.lower()
-        # Uncomment the next line for print() debugging
-        #print("Incrementing count for first_char=",first_char)
+        if first_char not in counts:
+            counts[first_char] = 0
         counts[first_char] += 1
+        print("Word {} has first character {} whose count is now {}".format(
+            word,
+            first_char,
+            counts[first_char]
+        ))
     return counts
 
 fn = sys.argv[1]
-with open(fn,"rt") as infile:
+with open(fn,"rt",encoding="UTF-8") as infile:
     inwords = [ line.strip() for line in infile ]
 
 hist = first_letter_hist( [ x for x in inwords if x!="" ] )
