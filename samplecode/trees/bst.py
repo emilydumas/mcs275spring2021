a737d3d7e4f9d0335dddc12aea766e7addca4115
David Dumas <david@dumas.io>
2021-02-26 11:03:31 -0600

    Split BST/binary tree stubs into separate modules

--- samplecode/trees/bst.py
+++ samplecode/trees/bst.py
@@ -1,8 +1,10 @@
-"""Binary trees and binary search trees"""
+"""Binary search trees"""
 # MCS 275 Spring 2021 - David Dumas
 # Lecture 20
 
-class Node:
-    """Node in a binary tree"""
+from binary import Node
+
+class BST:
+    """Binary search tree"""
     def __init__(self):
         pass
