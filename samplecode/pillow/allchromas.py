0cc7fc5cf9d6775fc567f0a555912051fd579ec7
David Dumas <david@dumas.io>
2021-03-09 23:56:11 -0600

    Python script to generate all chromaticities

---
+++ samplecode/pillow/allchromas.py
@@ -0,0 +1,18 @@
+"""Show all chromaticities in a bitmap image"""
+# MCS 275 Spring 2021 Lecture 24
+# David Dumas
+
+from PIL import Image
+
+img = Image.new("RGB",(256,256),color=(0,0,0))
+
+for x in range(256):
+    for y in range(256):
+        r = x
+        g = y
+        b = 255 - x - y
+        if b < 0:
+            continue
+        img.putpixel( (x,y), (r,g,b) )
+
+img.save("chromaticities.png")
